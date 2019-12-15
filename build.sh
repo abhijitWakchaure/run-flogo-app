b309b05a97238fceb6f4871858606b22b1336dc9
Abhijit Wakchaure <awakchau@tibco.com>
2019-12-15 10:19:40 +0530

    updated: the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "### Building for platform: linux/amd64"
-env GOOS=linux GOARCH=amd64 go build -o dist/linux/run-flogo-app
+env GOOS=linux GOARCH=amd64 go build -o dist/run-flogo-app-linux-amd64
 echo "### Building for platform: windows/amd64"
-env GOOS=windows GOARCH=amd64 go build -o dist/windows/run-flogo-app.exe
+env GOOS=windows GOARCH=amd64 go build -o dist/run-flogo-app-windows-amd64.exe
 echo "### Building for platform: darwin/amd64"
-env GOOS=darwin GOARCH=amd64 go build -o dist/darwin/run-flogo-app
+env GOOS=darwin GOARCH=amd64 go build -o dist/run-flogo-app-darwin-amd64
