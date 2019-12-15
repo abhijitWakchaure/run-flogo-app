ad6605773f55ab9512973c7993eba0354c694fc0
Abhijit Wakchaure <awakchau@tibco.com>
2019-12-15 16:09:26 +0530

    added: config support

--- main.go
+++ main.go
@@ -2,22 +2,104 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"os/user"
 	"path"
 	"regexp"
 	"runtime"
 	"sort"
+	"strings"
 )
 
 // Constants for local env
 const (
-	AppDir     = "/home/abhijit/Downloads"
-	AppPattern = `^.+-linux_amd64.*$`
+	DefaultAppPattern = `^.+-linux_amd64.*$`
+	ConfigFile        = ".run-flogo-app"
 )
+
+// App holds the environmet variables for the user
+type App struct {
+	AppDir     string `json:"rfAppDir" binding:"required"`
+	AppPattern string `json:"rfAppPattern"`
+}
+
+func (a *App) init() {
+	a.ReadConfig()
+	a.validateConfig()
+}
+
+// ReadConfig will read the config from configuration file
+func (a *App) ReadConfig() {
+	fileExists, err := checkFileExists(ConfigFile)
+	if err != nil {
+		log.Fatalln("# Error: ERR_READ_CONFIG", err)
+	}
+	if !fileExists {
+		fmt.Print("#> Creating config file...")
+		a.WriteConfig()
+		return
+	}
+	f, err := ioutil.ReadFile(ConfigFile)
+	if err != nil {
+		fmt.Println("#> Unable to read config...ignoring config...using defaults")
+		a.loadDefaultConfig()
+		return
+	}
+	err = json.Unmarshal(f, &a)
+	if err != nil {
+		fmt.Print("#> Invalid config detected...rewriting config...")
+		a.WriteConfig()
+	}
+}
+
+// WriteConfig will write the config into file
+func (a *App) WriteConfig() {
+	a.loadDefaultConfig()
+	configJSON, _ := json.MarshalIndent(a, "", "\t")
+	err := ioutil.WriteFile(ConfigFile, configJSON, 0600)
+	if err != nil {
+		log.Fatalln("# Error: ERR_WRITE_CONFIG", err)
+	}
+	fmt.Println("done")
+}
+
+func (a *App) loadDefaultConfig() {
+	fmt.Print("loading default config...")
+	a.AppDir = path.Join(GetUserHomeDir(), "Downloads")
+	a.AppPattern = DefaultAppPattern
+}
+
+func (a *App) validateConfig() {
+	if a.AppDir == "" {
+		fmt.Print("#> Invalid config detected...")
+		a.WriteConfig()
+	}
+}
+
+func checkFileExists(path string) (bool, error) {
+	if _, err := os.Stat(path); err == nil {
+		return true, nil
+	} else if os.IsNotExist(err) {
+		return false, nil
+	} else {
+		return false, err
+	}
+}
+
+// GetUserHomeDir returns the Home Directory of the user
+func GetUserHomeDir() string {
+	u, err := user.Current()
+	if err != nil {
+		log.Fatalln("# Error: ERR_GET_HOMEDIR", err)
+	}
+	return u.HomeDir
+}
 
 // EnableDebugLogs will add env variable to enable debug logs
 func EnableDebugLogs(cmd *exec.Cmd) *exec.Cmd {
@@ -27,62 +109,69 @@ func EnableDebugLogs(cmd *exec.Cmd) *exec.Cmd {
 }
 
 // FindLatestApp will return the latest flogo app name
-func FindLatestApp() string {
-	files, err := ioutil.ReadDir(AppDir)
+func (a *App) FindLatestApp() string {
+	files, err := ioutil.ReadDir(a.AppDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 	sort.SliceStable(files, func(i, j int) bool {
 		return files[i].ModTime().After(files[j].ModTime())
 	})
-	validApp := regexp.MustCompile(AppPattern)
+	validApp := regexp.MustCompile(a.AppPattern)
 	for _, f := range files {
 		if !f.IsDir() && validApp.MatchString(f.Name()) {
-			return path.Join(AppDir, f.Name())
+			return path.Join(a.AppDir, f.Name())
 		}
 	}
-	log.Println("# Info: No flogo apps found in " + AppDir)
+	log.Println("#> Info: No flogo apps found in " + a.AppDir)
 	return ""
 }
 
 // MakeAppExecutable will make the app executable
 func MakeAppExecutable(app string) {
+	fmt.Println("#> Making app executable...")
 	err := os.Chmod(app, 500)
 	if err != nil {
-		log.Fatalln("# Error:", err)
+		log.Fatalln("# Error: ERR_MAKE_APP_EXEC", err)
 	}
 }
 
 // RunFlogoApp will run the app
-func RunFlogoApp(app string, args []string) {
-	cmd := exec.Command(app)
+func RunFlogoApp(app string, debug *bool, tail []string) {
+	cmd := exec.Command(app, tail...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
-	if len(args) > 1 {
+	fmt.Println("#> Executing: ", strings.Join(cmd.Args, " "))
+	if *debug {
 		cmd = EnableDebugLogs(cmd)
 	}
 	err := cmd.Run()
 	if err != nil {
-		log.Fatalln("# Error:", err)
+		log.Fatalln("# Error: ERR_RUN_FA", err)
 	}
 }
 
 func main() {
-	app := FindLatestApp()
-	if len(app) > 0 {
-		fmt.Print("#> Do you want to execute \"", app, "\" [Y/n]: ")
+	app := App{}
+	app.init()
+	latestFlogoApp := app.FindLatestApp()
+	flagDebug := flag.Bool("debug", false, "Set this to enable debug logs")
+	flag.Parse()
+
+	if len(latestFlogoApp) > 0 {
+		fmt.Print("#> Do you want to execute this app \"", latestFlogoApp, "\" [Y/n]: ")
 		reader := bufio.NewReader(os.Stdin)
 		char, _, err := reader.ReadRune()
 		if err != nil {
-			log.Fatalln("# Error:", err)
+			log.Fatalln("# Error: ERR_READ_USRIN", err)
 		}
 		if char == 'Y' || char == 'y' {
 			if runtime.GOOS == "windows" {
 				// TODO: Handle for Windows
 			}
-			MakeAppExecutable(app)
-			RunFlogoApp(app, os.Args)
+			MakeAppExecutable(latestFlogoApp)
+			RunFlogoApp(latestFlogoApp, flagDebug, flag.Args())
 		} else {
 			log.Println("# Info: Exiting...")
 		}
