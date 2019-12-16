b249a6c0615a64147e684899d15377e2fed6ef8a
Abhijit Wakchaure <abhijitwakchaure.2014@gmail.com>
2019-12-17 01:20:18 +0530

    updated: build script to reduce build size

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "### Building for platform: linux/amd64"
-env GOOS=linux GOARCH=amd64 go build -o dist/run-flogo-app-linux-amd64
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o dist/run-flogo-app-linux-amd64
 echo "### Building for platform: windows/amd64"
-env GOOS=windows GOARCH=amd64 go build -o dist/run-flogo-app-windows-amd64.exe
+env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o dist/run-flogo-app-windows-amd64.exe
 echo "### Building for platform: darwin/amd64"
-env GOOS=darwin GOARCH=amd64 go build -o dist/run-flogo-app-darwin-amd64
+env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o dist/run-flogo-app-darwin-amd64
